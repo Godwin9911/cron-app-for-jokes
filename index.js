baa291c6fb6e24245d463107784822c4c94af5aa
Godwin Agedah <agedah99@gmail.com>
2019-05-12 01:53:42 +0100

    removing the creation of the server
    testing only the script

--- index.js
+++ index.js
@@ -2,17 +2,12 @@ const dotenv = require('dotenv');
 dotenv.config();
 const accountSid = process.env.ACCOUNT_SID;
 const authoToken = process.env.AUTH_TOKEN;
-const http = require('http');
 const client = require('twilio')(accountSid, authoToken);
 const schedule = require('node-schedule');
 require('es6-promise').polyfill();
 require('isomorphic-fetch');//use nodefetch
 
-//const port = process.env.PORT || 3000
-
-//http.createServer((req, res) => {
-  //cron job at 6:00am
- const j = schedule.scheduleJob('0 0 9 ? * *', () => {
+const j = schedule.scheduleJob('0 0 9 ? * *', () => {
     //get random jokes from Api
     fetch('https://api.chucknorris.io/jokes/random')
     .then(res => res.json())
@@ -26,7 +21,4 @@ require('isomorphic-fetch');//use nodefetch
       .then(message => console.log(message.sid));
     })
     .catch((error) => console.log(error))
- })
-/*}).listen(port, () => {
-  console.log(`server started on Port ${port}`);
-})*/
+})
