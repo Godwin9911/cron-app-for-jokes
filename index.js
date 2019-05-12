5ca22ee6dcc9d9e59697110ae41ce6e8cb76cb0e
Godwin Agedah <agedah99@gmail.com>
2019-05-12 02:49:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ const accountSid = process.env.ACCOUNT_SID;
 const authoToken = process.env.AUTH_TOKEN;
 const client = require('twilio')(accountSid, authoToken);
 const express = require('express');
-const schedule = require('node-schedule');
+const schedule = require('node-schedule-tz');
 require('es6-promise').polyfill();
 require('isomorphic-fetch');//use nodefetch
 
 const app = express()
 const port = process.env.PORT || 3000
       
-//cron job for 9 am everyday
-const j = schedule.scheduleJob('0 0 9 ? * *', () => {
+//cron job for 8 am everyday
+const j = schedule.scheduleJob('0 0 8 ? * *','Africa/Lagos', () => {
     //get random jokes from Api
     fetch('https://api.chucknorris.io/jokes/random')
     .then(res => res.json())
