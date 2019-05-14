0178418fc9d9b2b5d4773e5b2b0a9c03c38ffda3
Godwin Agedah <agedah99@gmail.com>
2019-05-14 21:50:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express()
 const port = process.env.PORT || 3000
       
 //cron job for 8 am everyday
-const j = schedule.scheduleJob('*/5 * * * *', () => {
+const j = schedule.scheduleJob('*/2 * * * *', () => {
     //get random jokes from Api
     fetch('https://api.chucknorris.io/jokes/random')
     .then(res => res.json())
